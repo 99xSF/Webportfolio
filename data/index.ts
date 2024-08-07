fcc1cee8f21b70be9f58a1788aaf579011dd0265
99xSF <samanfatima786@outlook.com>
2024-08-07 14:03:24 -0400

    Added my first edited file

---
+++ data/index.ts
@@ -0,0 +1,180 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize detail orientation, collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "./b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm flexible with all opportunities",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve and learn",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a AgeWell program for yoga at home",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "./b5.svg",
+    spareImg: "./grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AgeWell",
+    des: "A holistic wellness platform designed to help individuals of all ages embrace a healthier lifestyle through Yoga/Physical Therapy.",
+    img: "./gsap.svg",
+    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./fm.svg"],
+  },
+  {
+    id: 2,
+    title: "BookReview",
+    des: "Developed a user-friendly platform where readers can post and share their thoughts on various books, fostering a community of literary enthusiasts.",
+    img: "./next.svg",
+    iconLists: ["./tail.svg", "./ts.svg", "./stream.svg", "./c.svg"],
+  },
+  {
+    id: 3,
+    title: "Educational Hangman",
+    des: "Established an interactive and collaborating educational hangman game using Java in J grasp to provide free educational resources to underserved children in Pakistan.",
+    img: "./p1.svg",
+    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./c.svg"],
+  },
+  {
+    id: 4,
+    title: "Tic-Tac-Toe",
+    des: "Created a study break management tool with versatile gaming experience, featuring both two-player mode and a responsive AI opponent with adjustable difficulty levels",
+    img: "./three.svg",
+    iconLists: ["./next.svg", "./tail.svg", "./ts.svg"],
+  },
+];
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "SWE Fellow @ HeadStarter AI",
+    desc: "Developed and implemented innovative AI solutions, contributing to the advancement of cutting-edge technology and driving impactful results.",
+    className: "md:col-span-2",
+    thumbnail: "./exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Break Through Tech AI @ Cornell Tech",
+    desc: "Contributed towards 12-month long program, including Machine Learning Certificate with Cornell faculty, experiential learning experiences, and membership from industry professionals.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "./exp2.svg",
+  },
+  {
+    id: 3,
+    title: "FutureMaker @ MIT",
+    desc: "Engaged in a comprehensive program focused on foundation AI and machine learning concepts, including a AI certificate.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "./exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Cybersecurity Intern Global Shalla @ Illinois Institute of Technology",
+    desc: "Collaborated with a global team to address cybersecurity challenges and improve overall security posture.",
+    className: "md:col-span-2",
+    thumbnail: "./exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "./git.svg",
+  },
+  {
+    id: 2,
+    img: "./twit.svg",
+  },
+  {
+    id: 3,
+    img: "./link.svg",
+  },
+];
